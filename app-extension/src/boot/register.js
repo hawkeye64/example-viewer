1313b16d9480471935f345189f980d3b725199e3
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-20 16:26:09 -0700

    chore: remove QCalendar and QRibbon as direct dependencies

--- app-extension/src/boot/register.js
+++ app-extension/src/boot/register.js
@@ -1,9 +1,3 @@
 import Vue from 'vue'
 import VuePlugin from 'quasar-ui-example-viewer'
 Vue.use(VuePlugin)
-
-import QRibbon from '@quasar/quasar-ui-qribbon'
-Vue.use(QRibbon)
-
-import QMarkdown from '@quasar/quasar-ui-qmarkdown'
-Vue.use(QMarkdown)
